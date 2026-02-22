d9b2bab6d1c2c98fa558a29794296cfe99fa58f4
Aniruddha <aniruddhak.speed17@gmail.com>
2026-02-22 18:38:53 +0530

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 
-# 🔗 MongoDB Atlas connection
+# MongoDB Atlas connection
 mongo_uri = os.environ.get("MONGO_URI")
 client = MongoClient(mongo_uri)
 db = client["student_db"]
@@ -31,6 +31,22 @@ def submit():
     except Exception as e:
         # Show error on same page
         return render_template('form.html', error=str(e))
+    
+# MongoDB Atlas connection
+client = MongoClient(os.getenv("MONGO_URI"))
+db = client["todo_db"]
+collection = db["todo_items"]
+
+@app.route('/submittodoitem', methods=['POST'])
+def submit_todo_item():
+    item_name = request.form.get("itemName")
+    item_desc = request.form.get("itemDescription")
+
+    if not item_name or not item_desc:
+        return jsonify({"error": "Missing fields"}), 400
+
+    collection.insert_one({"itemName": item_name, "itemDescription": item_desc})
+    return "Item submitted successfully!"
 
 @app.route('/success')
 def success():
